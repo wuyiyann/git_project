ed4d3f5b6440319f1bea96a96f152983b5114c5b
wuyiyann2019 <482828544@qq.com>
2020-06-09 15:28:41 +0800

    第一次提交

---
+++ student_prc/src/student_prc/StudentModel.java
@@ -0,0 +1,15 @@
+package student_prc;
+
+import java.util.List;
+
+public class StudentModel {
+	public void addUser(int id,String username,String password) {
+	}
+	public void deleteUser (int id) {
+	}
+	public void updateUser(int id,String newUsername,String newPassword) {
+	}
+    public List<Object> scanUser(int pageIndex,int pageSize){
+    	return null;
+    }
+}
